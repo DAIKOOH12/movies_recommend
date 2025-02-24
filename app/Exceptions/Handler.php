e30f59d130159818ec1d308767fb67ee50aec6a0
Truong <daikooh1234@gmail.com>
2025-02-25 00:34:24 +0700

    Cap nhat tu dong redirect khi bao loi

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +29,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException || $exception instanceof NotFoundHttpException) {
+            return redirect()->route('notfound');
+        }
+        if($exception instanceof HttpException && $exception->getStatusCode() == 500){
+            return redirect()->route('notfound');
+        }
+        return parent::render($request, $exception);
     }
 }
